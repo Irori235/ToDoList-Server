f318e0d72006dd72762b2dfdd75c2226f37a0654
Irori235 <hiro030313@gmail.com>
2022-03-21 13:15:33 +0900

    Fix error handling

--- router/task.go
+++ router/task.go
@@ -9,15 +9,15 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// 関数　GetTasksHandlerは引数がecho.Context型のc で、戻り値はerror型である
+// 関数 GetTasksHandlerは引数がecho.Context型のc で、戻り値はerror型である
 func GetTasksHandler(c echo.Context) error {
 
-	//　model(package)の関数GetTasksを実行し、戻り値をtasks,errと定義する。
+	// model(package)の関数GetTasksを実行し、戻り値をtasks,errと定義する。
 	tasks, err := model.GetTasks()
 
 	// errが空でない時は StatusBadRequest(*5) を返す
 	if err != nil {
-		return c.NoContent(http.StatusBadRequest)
+		return echo.NewHTTPError(http.StatusBadRequest, "Bad Request")
 	}
 
 	// StasusOK と tasksを返す
