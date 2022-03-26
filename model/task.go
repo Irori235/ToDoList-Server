331e361fe1510b97795e66ca5be7ebe4235036de
Irori235 <hiro030313@gmail.com>
2022-03-26 11:32:50 +0900

    Add post,put,delete API

--- model/task.go
+++ model/task.go
@@ -24,3 +24,40 @@ func GetTasks() ([]Task, error) {
 	// tasksとerrを返す
 	return tasks, err
 }
+
+// 関数 AddTask は引数がstring型のnameで、戻り値はTaskのポインターとerror型である (*1)
+func AddTask(name string) (*Task, error) {
+
+	// 新たなuuidを生成し、これをid、成否をerrとする（*2）
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	// ID,Name,Finishedにid,name,false を代入したTask型のtaskを定義
+	task := Task{
+		ID:       id,
+		Name:     name,
+		Finished: false,
+	}
+
+	// taskをDBのTaskテーブルに追加。その成否を(ry
+	err = db.Create(&task).Error
+
+	// taskのポインタ と errを返す
+	return &task, err
+}
+
+// 関数 ChangeFinishedTaskの引数はuuid.UUID型のtaskIDで、戻り値はerror型である
+func ChangeFinishedTask(taskID uuid.UUID) error {
+
+	// DBのTaskテーブルからtaskIDと一致するidを探し、そのFinishedをtureにする(*3)
+	err := db.Model(&Task{}).Where("id = ?", taskID).Update("finished", true).Error
+	return err
+}
+
+func DeleteTask(taskID uuid.UUID) error {
+	// DBのTaskテーブルからtaskIDと一致するidを探し、そのタスクを削除する
+	err := db.Where("id = ?", taskID).Delete(&Task{}).Error
+	return err
+}
